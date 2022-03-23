f195e9d0d2e04d77502e702b9ac7a7939569163f
Ethelyn Interdictor <einterdi@si-a3.kzn.21-school.ru>
2022-03-24 00:32:49 +0300

    day01

--- Day00/ex04/day00_ex04.sql
+++ Day00/ex04/day00_ex04.sql
@@ -1,4 +1,4 @@
 SELECT
 	concat(name, ' (age:', age, ', gender:‘', gender, '’, address:‘', address,'’)')
-	as person_information FROM person
+	AS person_information FROM person
 Order by person_information
