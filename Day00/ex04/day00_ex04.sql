7b9f6ec867956fbd2fa2f1d25781c8137cbd5a09
Ethelyn Interdictor <einterdi@si-a3.kzn.21-school.ru>
2022-03-24 15:41:37 +0300

    other solve

--- Day00/ex04/day00_ex04.sql
+++ Day00/ex04/day00_ex04.sql
@@ -2,3 +2,11 @@ SELECT
 	concat(name, ' (age:', age, ', gender:‘', gender, '’, address:‘', address,'’)')
 	AS person_information FROM person
 Order by person_information;
+
+
+SELECT
+    name || ' (age:' || age || ', gender:''' || gender || ''', address:''' || address || ''')' AS person_information
+FROM
+    person
+ORDER BY
+    person_information ASC;
