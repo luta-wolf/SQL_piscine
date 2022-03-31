9ddc6ec230332529654066f44d9ce9ce80bbe5e3
Ethelyn Interdictor <einterdi@si-b2.kzn.21-school.ru>
2022-03-31 23:36:24 +0300

    add files day06

---
+++ Day06/src/ex05/day06_ex05.sql
@@ -0,0 +1,5 @@
+COMMENT ON TABLE person_discounts IS 'Это таблица скидок жи есть';
+COMMENT ON COLUMN person_discounts.id IS 'Это айди';
+COMMENT ON COLUMN person_discounts.fk_person_discounts_person_id IS 'Это Имя мудака, которому скидку ебашим';
+COMMENT ON COLUMN person_discounts.fk_person_discounts_pizzeria_id IS 'Это Имя нашей офигенной пиццерии';
+COMMENT ON COLUMN person_discounts.discount IS 'Это наша неебическая скидка';
