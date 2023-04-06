f2f316b9ea6c5b1d4e83fc072cfdbbbed78347aa
luta-wolf <denis.skrynnikov.job@gmail.com>
2023-04-06 19:14:03 +0300

    day02

--- Day02/src/ex01/day02_ex01.sql
+++ Day02/src/ex01/day02_ex01.sql
@@ -1,7 +1,13 @@
-SELECT DISTINCT visit_d as missing_date
-FROM (SELECT gs::date AS visit_d
-FROM generate_series('2022-01-01', '2022-01-10','1 day'::interval) AS gs) t
-LEFT JOIN (SELECT visit_date FROM person_visits WHERE (person_id = 1 OR person_id = 2 )) t1
-ON t1.visit_date = visit_d
-WHERE visit_date is null
-ORDER BY visit_d;
+SELECT DISTINCT pv2.visit_date AS missing_date FROM person_visits pv2
+LEFT JOIN person_visits pv1 ON pv1.visit_date=pv2.visit_date
+AND (pv1.person_id =1 OR pv1.person_id =2)
+WHERE pv1.person_id IS NULL
+ORDER BY 1;
+
+-- SELECT DISTINCT visit_d as missing_date
+-- FROM (SELECT gs::date AS visit_d
+-- FROM generate_series('2022-01-01', '2022-01-10','1 day'::interval) AS gs) t
+-- LEFT JOIN (SELECT visit_date FROM person_visits WHERE (person_id = 1 OR person_id = 2 )) t1
+-- ON t1.visit_date = visit_d
+-- WHERE visit_date is null
+-- ORDER BY visit_d;
--- Day02/src/ex09/day02_ex09.sql
+++ Day02/src/ex09/day02_ex09.sql
@@ -6,7 +6,19 @@ WITH table1 AS (
 	LEFT JOIN pizzeria ON pizzeria.id = pizzeria_id
 	WHERE person.gender = 'female'
 )
-SELECT person_name
-FROM table1
+SELECT person_name FROM table1
 WHERE pizza_name = 'cheese pizza' AND person_name IN (SELECT person_name FROM table1 WHERE pizza_name = 'pepperoni pizza')
 ORDER BY person_name;
+
+-- SELECT person.name FROM person
+-- LEFT JOIN person_order ON person.id = person_order.person_id
+-- LEFT JOIN menu ON person_order.menu_id = menu.id
+-- LEFT JOIN pizzeria ON menu.pizzeria_id = pizzeria.id
+-- WHERE gender='female' AND menu.pizza_name='pepperoni pizza'
+-- INTERSECT
+-- SELECT person.name FROM person
+-- LEFT JOIN person_order ON person.id = person_order.person_id
+-- LEFT JOIN menu ON person_order.menu_id = menu.id
+-- LEFT JOIN pizzeria ON menu.pizzeria_id = pizzeria.id
+-- WHERE gender='female' AND menu.pizza_name='cheese pizza'
+-- ORDER BY name
